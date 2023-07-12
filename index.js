c97cf6dadb8e3468c1f136283ec6ff126865308f
Anna Muroshnuk <amorosnik@36gmail.com>
2023-07-12 13:51:20 +0000

    :index.js was changed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const Datastore = requers('nebd');
-const lat = requre('data');
+const Datastore = requer('nebd');
+const lat = requre('data'); 
 const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('listenin at 3000')):
@@ -9,26 +9,26 @@ app.use(express.json{limit: '1mg' }));
 
 const database = new Datastore('database.db');
 database.loadDatabase();
-database.insert({name: 'AnnaMyroshnyk, status: '🌈'});
-database.insert({name: 'Hanna, status: '🌎'});
 
 
-app.post('/api',(request, response) => {
-  console.log('I got a reguest!');
-  console.log(request.body);
 
+app.get('/api',(reque, response) => {
+  database.find({}, (err, datd) => {
+    if (err) {
+      response.end();
+      return;
+    }
+    response.json(data);
+});
+});
+app.post('/api',(request, response) =>{
   const data = reguest.body;
-  const timestamp = Date.now()p
+  const timestamp = Date.now();
   data.time stamp = timestamp;
   database.insert(data);
+  response.json(data);
 
-  response.json ({
-  status: 'success',
-  timestamp: timestamp,
-  latitude: data.lat,
-  longitude: data.lon,
  });
-});
    
 
 
